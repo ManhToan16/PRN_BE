4638f08f5f7aa6cd7db6c031a97fdacd103e9990
ManhToan16 <toanluon507@gmail.com>
2025-04-19 11:12:31 +0700

    add new Migration

--- PRN_BE/Data/Room.cs
+++ PRN_BE/Data/Room.cs
@@ -1,7 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PRN_BE.Data
 {
+
     [Table("Room")]
     public class Room
     {
