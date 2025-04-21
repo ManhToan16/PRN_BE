73fb3d9ab91bb5bc43c8f8d904f87a29c95db9e7
Skuukzky30 <hoangphihung.hn2002@gmail.com>
2025-04-21 08:24:04 +0700

    media model + repo

---
+++ PRN_BE/Controllers/MediaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PRN_BE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MController : ControllerBase
+    {
+        
+    }
+}
