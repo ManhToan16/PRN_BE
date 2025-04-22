331831302e108dee3fb3a7254278a9a8887eb919
tuanddhe-151088 <tuanddhe151088@fpt.edu.vn>
2025-04-22 09:59:36 +0700

    Update Models Request For Room

---
+++ PRN_BE/Models/Request/RoomModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PRN_BE.Models.Request
+{
+    public class RoomModel
+    {
+        public int room_id { get; set; }
+        [Required] public string room_number { get; set; }
+
+        [Required]
+        [Range(0, double.MaxValue, ErrorMessage = "Price must be greater than or equal to 0.")]
+        public double price_per_night { get; set; }
+        [Required] public string description { get; set; }
+        [Required] public int room_status_id { get; set; }
+        [Required] public string location { get; set; }
+        public int num_beds { get; set; }
+
+        public int num_bathrooms { get; set; }
+        public String? user_id { get; set; }
+        public string? thumbnail { get; set; }
+    }
+}
+}
