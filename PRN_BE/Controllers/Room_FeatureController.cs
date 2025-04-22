8594697e9fb0ad69a6173f452aabe5a43f1590cd
tuanddhe-151088 <tuanddhe151088@fpt.edu.vn>
2025-04-22 10:21:43 +0700

    Create Room_FeatureController

---
+++ PRN_BE/Controllers/Room_FeatureController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PRN_BE.Models.Request;
+using PRN_BE.Repositories;
+
+namespace PRN_BE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Room_FeatureController : ControllerBase
+    {
+        public class RFController : ControllerBase
+        {
+            private readonly IRoom_FeatureRepository _room_feature_repository;
+
+            public RFController(IRoom_FeatureRepository room_feature_repository)
+            {
+                _room_feature_repository = room_feature_repository;
+            }
+
+            [HttpPut("UpdateFeature")]
+            public async Task<IActionResult> UpdateRoomFeatureIds([FromBody] UpdateRoomFeatureModel model)
+            {
+                var result = await _room_feature_repository.UpdateRoomFeatureIds(model);
+                return result.StatusCode == 200 ? Ok(result) : BadRequest(result);
+            }
+
+        }
+
+    }
+}
