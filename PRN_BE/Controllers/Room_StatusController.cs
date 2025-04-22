1b7d44a0631cea98f371635d41a972e3070832b5
tuanddhe-151088 <tuanddhe151088@fpt.edu.vn>
2025-04-22 10:27:21 +0700

    Update Room_StatusController.cs

--- PRN_BE/Controllers/Room_StatusController.cs
+++ PRN_BE/Controllers/Room_StatusController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PRN_BE.Models.Request;
+using PRN_BE.Repositories;
 
 namespace PRN_BE.Controllers
 {
@@ -7,5 +10,51 @@ namespace PRN_BE.Controllers
     [ApiController]
     public class Room_StatusController : ControllerBase
     {
+        private readonly IRoom_StatusRepository _roomStatusRepository;
+
+        public Room_StatusController(IRoom_StatusRepository roomStatusRepository)
+        {
+            _roomStatusRepository = roomStatusRepository;
+        }
+
+        [HttpGet("All")]
+        public async Task<IActionResult> GetAllRoom_Status()
+        {
+            var result = await _roomStatusRepository.GetAllRoom_Status();
+            return result.StatusCode == 200 ? Ok(result) : NotFound(result);
+        }
+
+        [Authorize]
+        [HttpPost("Create")]
+        public async Task<IActionResult> CreateRoom_Status([FromBody] Room_StatusModel model)
+        {
+            var result = await _roomStatusRepository.CreateRoom_Status(model);
+            return result.StatusCode == 200 ? Ok(result) : BadRequest(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetRoom_StatusById(int id)
+        {
+            var result = await _roomStatusRepository.GetRoom_StatusById(id);
+            return result.StatusCode == 200 ? Ok(result) : NotFound(result);
+        }
+        [Authorize]
+        [HttpPut("Update/{id}")]
+        public async Task<IActionResult> UpdateRoom_Status(int id, [FromBody] Room_StatusModel model)
+        {
+            var result = await _roomStatusRepository.UpdateRoom_Status(id, model);
+            return result.StatusCode == 200 ? Ok(result) : NotFound(result);
+        }
+
+        [Authorize]
+        [HttpDelete("Delete/{id}")]
+        public async Task<IActionResult> DeleteRoom_Status(int id)
+        {
+            var result = await _roomStatusRepository.DeleteRoom_Status(id);
+            return result.StatusCode == 200 ? Ok(result) : NotFound(result);
+        }
+
+
+
     }
 }
