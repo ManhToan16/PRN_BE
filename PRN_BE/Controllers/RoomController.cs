1f1d2e4bed573ab3d9499070c5b60cbd5004efa6
Zariel <zariel.nd@gmail.com>
2025-04-24 10:20:48 +0700

    add endpoint for manage place

--- PRN_BE/Controllers/RoomController.cs
+++ PRN_BE/Controllers/RoomController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PRN_BE.Models.Request;
 using PRN_BE.Repositories;
 
 namespace PRN_BE.Controllers
@@ -28,7 +30,48 @@ namespace PRN_BE.Controllers
             var result = await _roomRepository.GetRoomByIdAsync(id);
             return result.StatusCode == 200 ? Ok(result) : NotFound(result);
         }
+        [Authorize]
+        [HttpPost("Create")]
+        public async Task<IActionResult> CreateRoom([FromBody] Room_FeatureIdsModel room)
+        {
+            var result = await _roomRepository.CreateRoomAsync(room);
+            return result.StatusCode == 200 ? Ok(result) : BadRequest(result);
+        }
 
+        [HttpPut("Update/{id}")]
+        public async Task<IActionResult> UpdateRoom(int id, [FromBody] RoomModel room)
+        {
+            var result = await _roomRepository.UpdateRoomAsync(id, room);
+            return result.StatusCode == 200 ? Ok(result) : BadRequest(result);
+        }
+
+        [HttpPost("Search")]
+        public async Task<IActionResult> SearchRoom(RoomSearchModel model)
+        {
+            var result = await _roomRepository.SearchRoomsByLocationAsync(model);
+            return result.StatusCode == 200 ? Ok(result) : NotFound(result);
+        }
+
+        [HttpPost("UpdateStatus/{id}")]
+        public async Task<IActionResult> RoomUpdateStatus(int id, [FromBody] RoomUpdateStatus model)
+        {
+            var result = await _roomRepository.UpdateRoomStatusAsync(id, model);
+            return result.StatusCode == 200 ? Ok(result) : BadRequest(result);
+        }
+
+        [HttpPost("GetByStatus/{statusId}")]
+        public async Task<IActionResult> RoomGetByStatus(int statusId)
+        {
+            var result = await _roomRepository.GetRoomByStatusIdAsync(statusId);
+            return result.StatusCode == 200 ? Ok(result) : NotFound(result);
+        }
+        [Authorize]
+        [HttpGet("GetRooms")]
+        public async Task<IActionResult> GetRooms()
+        {
+            var result = await _roomRepository.GetRoomsByUserIdAsync();
+            return result.StatusCode == 200 ? Ok(result) : NotFound(result);
+        }
 
     }
 }
