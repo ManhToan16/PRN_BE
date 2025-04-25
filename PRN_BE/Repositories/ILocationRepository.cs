fa507d3933b2211795b16e97e2d36d2c4f57a0d7
Zariel <zariel.nd@gmail.com>
2025-04-25 22:53:59 +0700

    add location interfaces

---
+++ PRN_BE/Repositories/ILocationRepository.cs
@@ -0,0 +1,15 @@
+﻿using PRN_BE.Models;
+using PRN_BE.Models.Response;
+
+namespace PRN_BE.Repositories;
+
+public interface ILocationRepository
+{
+    public Task<BaseDataResponse<List<LocationModel>>> GetLocationsAsync();
+    public Task<BaseDataResponse<LocationModel>> GetLocationByIdAsync(int id);
+    public Task<BaseDataResponse<object>> CreateLocationAsync(LocationModel location);
+    public Task<BaseDataResponse<object>> UpdateLocationAsync(int id, LocationModel location);
+    public Task<BaseDataResponse<object>> DeleteLocationAsync(int id);
+    public Task<BaseDataResponse<List<LocationModel>>> SearchLocationAsync(LocationModel location);
+    
+}
